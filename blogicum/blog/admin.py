4dbdf5a073805aeca0e69c337f0954d425880f96
fcklck <145225498+fcklck@users.noreply.github.com>
2025-03-19 18:37:42 +0500

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,57 +1,20 @@
 from django.contrib import admin
-from blog.constants import NUM_OF_WORDS_OF_TEXT, NUM_OF_WORDS_OF_TITLE
-from blog.models import Category, Comment, Location, Post
-from django.utils.html import format_html
+from .models import Category, Location, Post
 
-admin.site.empty_value_display = 'Не задано'
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_published', 'created_at')
+    search_fields = ('title', 'slug')
+    list_filter = ('is_published',)
 
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('is_published',)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """Класс для настройки интерфейса админ-зоны модели Post."""
-
-    list_display = (
-        'get_short_title',
-        'get_short_text',
-        'pub_date',
-        'category',
-        'is_published',
-        'get_comment_count',
-        'image_tag',
-    )
-    list_editable = (
-        'is_published',
-        'pub_date',
-        'category'
-    )
-    search_fields = ('title', 'text',)
-    list_filter = ('category', 'location',)
-
-    @admin.display(description='Заголовок')
-    def get_short_title(self, obj):
-        """Получаем начальные слова заголовка поста."""
-        return " ".join(obj.title.split()[:NUM_OF_WORDS_OF_TITLE])
-
-    @admin.display(description='Описание')
-    def get_short_text(self, obj):
-        """Получаем начальные слова текста описания поста."""
-        return f'{" ".join(obj.text.split()[:NUM_OF_WORDS_OF_TEXT])} ...'
-
-    @admin.display(description='Комментарии')
-    def get_comment_count(self, obj):
-        """Получаем колличество комментариев в посте."""
-        return obj.comments.count()
-
-    @admin.display(description='Изображение')
-    def image_tag(self, obj):
-        """Получаем картинку к посту."""
-        if obj.image:
-            return format_html(
-                '<img src="{}" width="50" height="50" />'.format(obj.image.url)
-            )
-        return 'Не найдено'
-
-
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Comment)
+    list_display = ('title', 'author', 'pub_date', 'is_published', 'created_at')
+    search_fields = ('title', 'author__username', 'category__title')
+    list_filter = ('is_published', 'category', 'location')
