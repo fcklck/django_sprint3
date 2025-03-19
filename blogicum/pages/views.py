b43c6ca1286244161cb4cbe85d89fe921261b02f
fcklck <145225498+fcklck@users.noreply.github.com>
2025-03-19 18:43:12 +0500

    Update views.py

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,27 +1,18 @@
+# pages/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-"""CBV для обработки Статичных страниц"""
-
-
-class RulesTemplateView(TemplateView):
-    template_name = 'pages/rules.html'
-
 
 class AboutTemplateView(TemplateView):
     template_name = 'pages/about.html'
 
-
-"""Функции для обработки ошибок"""
-
+class RulesTemplateView(TemplateView):
+    template_name = 'pages/rules.html'
 
 def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
 
-
 def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
-
 
 def server_error(request):
     return render(request, 'pages/500.html', status=500)
